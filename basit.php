497b178e67f1a1f0d312255321a258fda58643ba
Afmha <afmha@hotmail.com>
2016-02-29 21:58:47 +0200

    Update basit.php

--- basit.php
+++ basit.php
@@ -32,11 +32,26 @@
 			}
 			$this->yis++; //Yapılan işlem sayısına bir sayı daha eklendi.
 		}
+		
+		public function dahil_et($a="",$b=0){
+			if(file_exists($a)){
+				include($a);
+				$this->yis++; //Yapılan işlem sayısına bir sayı daha eklendi.
+			}else{
+				if($b > 0){
+					echo $a.' dosyası bulunamadı.';
+					$this->yis++; //Yapılan işlem sayısına bir sayı daha eklendi.
+				}else{
+					
+				}
+			}
+		}
+		
 		public function yis(){
 			$this->yis++; //Yapılan işlem sayısına bir sayı daha eklendi.
 			echo $this->yis;
 		}
 		
 	}
-
+	
 ?>
